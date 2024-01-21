ef92a0c0cba3cfae4996113d33379bd2aa585438
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-21 20:41:27 +0900

    10171 - 고양이

---
+++ baekjoon/Newbie/10171/10171/10171.cpp
@@ -0,0 +1,21 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 10171.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 10171 - 고양이
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    cout << "\\    /\\\n";
+    cout << " )  ( \')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|";
+}
