4f4926e4ffaf8c8163824ef8a0e3d924693ce438
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-22 00:15:38 +0900

    25083 - 새싹

---
+++ baekjoon/Newbie/25083/25083/25083.cpp
@@ -0,0 +1,25 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 25083.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 25083 - 새싹
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    cout << "         ,r\'\"7\n";
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+
+    return 0;
+}
