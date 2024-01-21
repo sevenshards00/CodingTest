2c3a6f1863fda537aaecf41dbf26f35eaa7fa2f6
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-21 20:29:42 +0900

    2557 - Hello World

---
+++ baekjoon/Newbie/2557/2557/2557.cpp
@@ -0,0 +1,20 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 2557.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 2557 - Hello World
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cout;
+
+int main()
+{
+	cout << "Hello World!";
+
+	return 0;
+}
