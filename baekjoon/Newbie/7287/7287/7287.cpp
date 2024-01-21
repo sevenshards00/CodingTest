84faafac74028c8acad32ed81a39375766b77fef
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-21 20:36:58 +0900

    7287 - 등록

---
+++ baekjoon/Newbie/7287/7287/7287.cpp
@@ -0,0 +1,20 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 7287.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 7287 - 등록
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    cout << "맞춘 문제 수\n" << "본인 닉네임";
+
+    return 0;
+}
