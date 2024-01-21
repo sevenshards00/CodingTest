3e91df29026ae8990d92208c8cc5b6c3176c854c
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-21 20:31:10 +0900

    2739 - 구구단

---
+++ baekjoon/Newbie/2739/2739/2739.cpp
@@ -0,0 +1,26 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 2739.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 2739 - 구구단
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main()
+{
+    int input;
+
+    cin >> input;
+
+    for (int i = 1; i != 10; ++i)
+        cout << input << " * " << i << " = " << input * i << '\n';
+
+    return 0;
+}
