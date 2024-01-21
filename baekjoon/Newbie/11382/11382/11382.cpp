c4ddc8b86f52aa5a6a2659fa6bec33b2df06d162
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-22 00:10:45 +0900

    11382 - 꼬마 정민

---
+++ baekjoon/Newbie/11382/11382/11382.cpp
@@ -0,0 +1,25 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 11382.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 11382 - 꼬마 정민
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    long long a;
+    long long b;
+    long long c;
+
+    cin >> a >> b >> c;
+    cout << a + b + c;
+    return 0;
+}
