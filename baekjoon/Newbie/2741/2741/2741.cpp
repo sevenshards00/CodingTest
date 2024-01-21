e1e84b9f0e9c7b8853cf6433ff2f87fcb640de01
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-21 20:33:10 +0900

    2741 - N 찍기

---
+++ baekjoon/Newbie/2741/2741/2741.cpp
@@ -0,0 +1,24 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 2741.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 2741 - N 찍기
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int a;
+    cin >> a;
+    for (int i = 1; i <= a; ++i)
+        cout << i << '\n';
+
+    return 0;
+}
