3f8f21353f098d172e217162a6132c748c8bfb07
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-22 00:08:02 +0900

    10998 - A x B

---
+++ baekjoon/Newbie/10998/10998/10998.cpp
@@ -0,0 +1,25 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 10998.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 10998 - A x B
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    
+    cin >> a >> b;
+    cout << a * b;
+
+    return 0;
+}
