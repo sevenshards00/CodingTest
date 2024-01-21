e354e93a88e1e717f1fd1c6d78a4322940c49b6a
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-22 00:04:04 +0900

    10951 - A + B - (4)

---
+++ baekjoon/Newbie/10951/10951/10951.cpp
@@ -0,0 +1,25 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 10951.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 10951 - A + B - (4)
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main()
+{
+    int a, b;
+    while (cin >> a >> b)
+    {
+        cout << a + b << '\n';
+    }
+
+    return 0;
+}
