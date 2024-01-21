abae46fe118ad64bff33a3b95d5dddb1b427744c
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-21 20:43:32 +0900

    10172 - 개

---
+++ baekjoon/Newbie/10172/10172/10172.cpp
@@ -0,0 +1,22 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 10172.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 10172 - 개
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    cout << "|\\_/|\n";
+    cout << "|q p|   /}\n";
+    cout << "( 0 )\"\"\"\\\n";
+    cout << "|\"^\"`    |\n";
+    cout << "||_/=\\\\__|";
+}
