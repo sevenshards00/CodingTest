3df66a4b11eb544416a686595ce7dcacf6289897
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-21 20:23:46 +0900

    1330 - 두 수 비교하기

---
+++ baekjoon/Newbie/1330/1330/1330.cpp
@@ -0,0 +1,31 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 1330.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 1330 - 두 수 비교하기
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+	int a;
+	int b;
+
+	cin >> a >> b;
+
+	if (a > b)
+		cout << ">";
+	else if (a < b)
+		cout << "<";
+	else
+		cout << "==";
+
+	return 0;
+}
