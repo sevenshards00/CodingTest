58628678f5af6a10064cb1f8891deeaf4f0e944c
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-21 20:54:18 +0900

    10869 - 사칙연산

---
+++ baekjoon/Newbie/10869/10869/10869.cpp
@@ -0,0 +1,29 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 10869.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 10869 - 사칙연산
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main()
+{
+    int a;
+    int b;
+
+    cin >> a >> b;
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b << '\n';
+    cout << a % b << '\n';
+
+    return 0;
+}
