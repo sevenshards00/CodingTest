b86c0e5cf05ae5c8f27a21f12c28e4724da04d76
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-21 20:35:05 +0900

    2753 - 윤년

---
+++ baekjoon/Newbie/2753/2753/2753.cpp
@@ -0,0 +1,27 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 2753.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-21
+* 이전 버전 작성 일자:
+* 버전 내용: 2753 - 윤년
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int year;
+    cin >> year;
+
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+        cout << 1;
+    else
+        cout << 0;
+
+    return 0;
+}
