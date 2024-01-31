d899e8d6cfff7f4dde0aa8b75db364b1877fe522
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-31 10:32:36 +0900

    11654 - 아스키 코드

---
+++ baekjoon/Newbie/11654/11654/11654.cpp
@@ -0,0 +1,23 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 11654.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-31
+* 이전 버전 작성 일자:
+* 버전 내용: 11654 - 아스키 코드
+* 이전 버전 내용:
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+
+int main()
+{
+	char input;
+	scanf("%c", &input);
+	printf("%d", input);
+
+	return 0;
+}
