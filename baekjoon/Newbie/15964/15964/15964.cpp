7d458f0c736f2d368c12a6f136185b81bcbb3e9e
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-31 10:49:40 +0900

    15964 - 이상한 기호

---
+++ baekjoon/Newbie/15964/15964/15964.cpp
@@ -0,0 +1,26 @@
+﻿/*
+* Coding Test - Baekjoon (새싹 난이도)
+* 파일명: 11654.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-31
+* 이전 버전 작성 일자:
+* 버전 내용: 15964 - 이상한 기호
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+	long long a;
+	long long b;
+
+	cin >> a >> b;
+
+	cout << (a + b) * (a - b) << '\n';
+
+	return 0;
+}
