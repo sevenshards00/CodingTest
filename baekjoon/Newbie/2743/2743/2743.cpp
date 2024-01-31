e5c58b1e195ac8e3096b461d5c2126ac00ab0750
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-31 10:56:05 +0900

    2743 - 단어 길이 재기

--- baekjoon/Newbie/2743/2743/2743.cpp
+++ baekjoon/Newbie/2743/2743/2743.cpp
@@ -1,26 +1,23 @@
 ﻿/*
 * Coding Test - Baekjoon (새싹 난이도)
-* 파일명: 15964.cpp
+* 파일명: 2743.cpp
 * 파일 버전: 0.1
 * 작성자: Sevenshards
 * 작성 일자: 2024-01-31
 * 이전 버전 작성 일자:
-* 버전 내용: 15964 - 이상한 기호
+* 버전 내용: 2743 - 단어 길이 재기
 * 이전 버전 내용:
 */
 
+#include <cstdio>
 #include <iostream>
 using std::cin;
 using std::cout;
+using std::string;
 
 int main()
 {
-	long long a;
-	long long b;
-
-	cin >> a >> b;
-
-	cout << (a + b) * (a - b) << '\n';
-
-	return 0;
+	string input;
+	cin >> input;
+	cout << input.length() << '\n';
 }
