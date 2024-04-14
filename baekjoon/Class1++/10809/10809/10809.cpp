9f307c859bc2bfb2feb5f90794e33519ffa875ad
Sevenshards <sevenshards@hanyang.ac.kr>
2024-04-15 08:31:10 +0900

    로직 개선

--- baekjoon/Class1++/10809/10809/10809.cpp
+++ baekjoon/Class1++/10809/10809/10809.cpp
@@ -1,12 +1,12 @@
 ﻿/*
 * Coding Test - Baekjoon
 * 파일명: 10809.cpp
-* 파일 버전: 0.1
+* 파일 버전: 0.11
 * 작성자: Sevenshards
 * 작성 일자: 2024-04-15
-* 이전 버전 작성 일자:
-* 버전 내용: 10809 - 알파벳 찾기
-* 이전 버전 내용:
+* 이전 버전 작성 일자: 2024-04-15
+* 버전 내용: 로직 개선
+* 이전 버전 내용: 10809 - 알파벳 찾기
 */
 
 #include <iostream>
@@ -16,28 +16,10 @@ using std::cout;
 #include <string>
 using std::string;
 
-#include <cstring>
-
 void AlphabetCounter(const string &str)
 {
-	char counter[26];
-
-	memset(counter, -1, sizeof(counter));
-
-	for (int idx = 0; idx != str.length(); ++idx)
-	{
-		for (char i = 'a'; i <= 'z'; ++i)
-		{
-			if ((counter[i % 'a'] == -1) && (str[idx] == i))
-			{
-				counter[i % 'a'] = idx;
-				break;
-			}
-		}
-	}
-
-	for (int i = 0; i != sizeof(counter) / sizeof(char); ++i)
-		printf("%d ", counter[i]);
+	for (char i = 'a'; i <= 'z'; ++i)
+		cout << static_cast<int>(str.find(i)) << ' ';
 }
 
 int main()
