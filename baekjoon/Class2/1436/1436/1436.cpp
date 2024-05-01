21aa4236c99b0047f9ca6aece31bb862b7938ee2
Sevenshards <sevenshards@hanyang.ac.kr>
2024-05-01 10:29:53 +0900

    1436 - 영화감독 숌

---
+++ baekjoon/Class2/1436/1436/1436.cpp
@@ -0,0 +1,51 @@
+﻿/*
+* Coding Test - Baekjoon (solved.ac - Class 2)
+* 파일명: 1436.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-05-01
+* 이전 버전 작성 일자:
+* 버전 내용: 1436 - 영화감독 숌
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+constexpr int SPECIFIC_NUM = 666;
+
+void CalcMovieTitle(int n)
+{
+	int title = 0;
+	int cnt = 0;
+	int temp;
+
+	while (n != cnt)
+	{
+		++title;
+		temp = title;
+
+		while (0 != temp)
+		{
+			if (SPECIFIC_NUM == temp % 1000)
+			{
+				++cnt;
+				break;
+			}
+			else
+				temp /= 10;
+		}
+	}
+
+	cout << title << '\n';
+}
+
+int main()
+{
+	int input;
+	cin >> input;
+	CalcMovieTitle(input);
+
+	return 0;
+}
