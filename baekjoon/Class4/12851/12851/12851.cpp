3bd960dc5217cab589026f4ed27bfe09e12093ee
Sevenshards <sevenshards@hanyang.ac.kr>
2024-05-20 10:31:26 +0900

    12851 - 숨바꼭질 2

---
+++ baekjoon/Class4/12851/12851/12851.cpp
@@ -0,0 +1,91 @@
+﻿/*
+* Coding Test - Baekjoon (solved.ac - Class 4)
+* 파일명: 12851.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-05-20
+* 이전 버전 작성 일자:
+* 버전 내용: 12851 - 숨바꼭질 2
+* 이전 버전 내용:
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+
+#include <queue>
+using std::queue;
+
+#include <cstring>
+
+constexpr int MAX_LEN = 100001;
+
+int g_Dir[3] = { 1, -1, 2 };
+
+void BFS(const int &ref_start, const int &ref_end)
+{
+	bool bVisited[MAX_LEN];
+	unsigned uElapsedTime[MAX_LEN];
+	unsigned currElapsedTime;
+	memset(bVisited, false, sizeof(bVisited));
+	memset(uElapsedTime, -1, sizeof(uElapsedTime));
+	int currPos = ref_start;
+	int nextPos;
+	int findWayCnt = 0;
+	queue<int> bfsQueue;
+
+	bfsQueue.push(currPos);
+	bVisited[currPos] = true;
+	uElapsedTime[currPos] = 0;
+
+	while (!bfsQueue.empty())
+	{
+		currPos = bfsQueue.front();
+		bfsQueue.pop();
+
+		if (currPos == ref_end)
+		{
+			++findWayCnt;
+			continue;
+		}
+
+		for (int i = 0; i != 3; ++i)
+		{
+			if (i < 2)
+			{
+				nextPos = currPos + g_Dir[i];
+				currElapsedTime = uElapsedTime[currPos] + 1;
+			}
+
+			else
+			{
+				nextPos = currPos * g_Dir[i];
+				currElapsedTime = uElapsedTime[currPos] + 1;
+			}
+
+			if (nextPos < 0 || nextPos >= MAX_LEN)
+				continue;
+
+			if (bVisited[nextPos] && currElapsedTime > uElapsedTime[nextPos])
+				continue;
+
+			uElapsedTime[nextPos] = currElapsedTime;
+			bVisited[nextPos] = true;
+			bfsQueue.push(nextPos);
+		}
+	}
+
+	printf("%d\n", uElapsedTime[ref_end]);
+	printf("%d\n", findWayCnt);
+}
+
+int main()
+{
+	int n;
+	int k;
+
+	scanf("%d %d", &n, &k);
+	BFS(n, k);
+
+	return 0;
+}
